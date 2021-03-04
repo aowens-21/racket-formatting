212550b8fa15141ee6d55c3278f91bd7af6f6121
shhyou <shu-hung.you@eecs.northwestern.edu>
2021-03-03 21:32:06 -0600

    Omit file-level API test input and output

--- custom-syntax-format/tests/custom-syntax-format/file-level.rkt
+++ custom-syntax-format/tests/custom-syntax-format/file-level.rkt
@@ -3,13 +3,19 @@
 (require rackunit
          racket/runtime-path
          custom-syntax-format
+         racket/port
          racket/file)
 
-(define-runtime-path file-level-conds.rkt "file-level-conds.rkt")
-(define-runtime-path file-level-expected.rkt "file-level-expected.rkt")
+(define-runtime-path file-level-conds.rkt "file-level/01-conds.rkt")
+(define-runtime-path file-level-expected.rkt "file-level/01-expected.rkt")
 
 (module+ test
   (require rackunit)
+
+  (check-equal? (with-output-to-string
+                  (λ () (dynamic-require file-level-conds.rkt #f)))
+                (with-output-to-string
+                  (λ () (dynamic-require file-level-expected.rkt #f))))
 
   (check-equal? (format-file file-level-conds.rkt)
                 (file->string file-level-expected.rkt)))
