16a5cc9de22c9529a959d76a4f8c04b0987beb08
Shu-Hung You <shu-hung.you@eecs.northwestern.edu>
2021-08-25 22:55:21 -0500

    Omit paths

---
+++ custom-syntax-format/custom-syntax-format/info.rkt
@@ -0,0 +1,5 @@
+#lang info
+
+(define temp-paths '("messy-editor-integration.rkt" "messy-editor-integration-animated.rkt"))
+(define compile-omit-paths temp-paths)
+(define test-omit-paths temp-paths)
